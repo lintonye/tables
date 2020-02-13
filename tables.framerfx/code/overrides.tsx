4329f8d7f83d65ab65ff285ebcd7f418fa777844
Linton Ye <linton@jimulabs.com>
2020-02-12 20:58:04 -0800

    Use componentForIndentifier

--- tables.framerfx/code/overrides.tsx
+++ tables.framerfx/code/overrides.tsx
@@ -1,25 +1,36 @@
-// import { componentLoader } from "framer"
+import { componentLoader } from "framer";
 
-let modules = null
+let modules = null;
 // let _overrideFileNames = []
 
-function initOverrides() {
-  if (modules === null && window.Vekter) {
-    const bundleNames = [
-      ...Object.keys(window.Vekter.engine.canvasStore.userScriptBundle)
-    ]
-    bundleNames
-      .reverse()
-      .forEach(n => eval(window.Vekter.engine.canvasStore.userScriptBundle[n]))
-    // eval(window.Vekter.engine.canvasStore.userScriptBundle["index.js"])
-    modules = module.exports.__framer__.sourceModules
+// const overrides = componentLoader
+//   .componentIdentifiers()
+//   .map(id => componentLoader.componentForIdentifier(id))
+//   .filter(component => component.type === "override")
+//   .map(c => c.class);
+
+// alert(overrides[0]);
+
+const visited = new Set();
+function findKey(root, keyNameRegex, path, result) {
+  if (typeof root === "object" && root !== null && !visited.has(root)) {
+    visited.add(root);
+    Object.keys(root)
+      .filter(k => k.match(keyNameRegex))
+      .forEach(k => result.push({ key: `${path}.${k}` }));
+    Object.keys(root).forEach(k =>
+      findKey(root[k], keyNameRegex, `${path}.${k}`, result)
+    );
   }
-  // const modules = module.exports.__framer__.sourceModules
-  // const appModule = modules["./App.tsx"]()
-  // const TextFun = appModule["Text"]
-  // alert(JSON.stringify(appModule.__info__[0]))
-  // alert(model.exports)
 }
+
+function debug() {
+  visited.clear();
+  const userScriptKeys = [];
+  findKey(window.Vekter, /.*Override.*/, `window.Vekter`, userScriptKeys);
+  alert(userScriptKeys.map(e => JSON.stringify(e)).join("\n"));
+}
+// alert(typeof window.Vekter);
 
 // export function overrideFileNames() {
 //   return modules
@@ -37,32 +48,31 @@ function initOverrides() {
 //   // } else return []
 // }
 
-function getModule(overrideFileName) {
-  // if (modules && overrideFileName) {
-  const moduleFactory = modules[overrideFileName]
-  return typeof moduleFactory === "function" && moduleFactory()
-  // }
-}
-
 export function getOverride(fileName, functionName) {
-  initOverrides()
-  // if (modules && fileName && functionName) {
-  const module = getModule(fileName)
-  // alert(Object.keys(module))
-  return module && module[functionName]
-  // }
+  // initOverrides();
+  const comp = componentLoader.componentForIdentifier(
+    `${fileName}_${functionName}`
+  );
+
+  return (
+    comp &&
+    comp.type === "override" &&
+    typeof comp.class === "function" &&
+    comp.class
+  );
 }
 
 export function getOverrideByComponentId(componentId) {
-  const regex = /^(\w+)(\.\/[\w\.]+)?/
-  const match = componentId.match(regex)
+  const regex = /^(\w+)(\.\/[\w\.]+)?/;
+  const match = componentId.match(regex);
   if (match) {
-    const node = window.Vekter.engine.tree.get(match[1])
+    const node = window.Vekter.engine.tree.get(match[1]);
     // alert(window.Vekter.engine.tree.idToNode)
     // alert("id=" + componentId + " node=" + node)
     if (node) {
-      const { codeOverrideFile, codeOverrideName } = node
-      return getOverride(codeOverrideFile, codeOverrideName)
+      // alert(Object.keys(node).join("\n"));
+      const { codeOverrideFile, codeOverrideName } = node;
+      return getOverride(codeOverrideFile, codeOverrideName);
     }
   }
 }
